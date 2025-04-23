798e019d80c7e92efff78c38ac011fd8e3235344
Anderson Junkes <ojunkes@gmail.com>
2025-04-23 17:40:07 -0300

    Configuração inicial camada api

--- src/Customer.Management.WebApi/Controllers/CustomerController.cs
+++ src/Customer.Management.WebApi/Controllers/CustomerController.cs
@@ -3,9 +3,17 @@ using Microsoft.AspNetCore.Mvc;
 namespace Customer.Management.WebApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route(RouteTemplate)]
+    [Produces("application/json")]
     public class CustomerController : ControllerBase
     {
+        private const string RouteTemplate = "api/v1/customers";
+
+        [HttpGet]
+        public Task<bool> GetAll()
+        {
+            return Task.FromResult(true);
+        }
 
     }
 }
