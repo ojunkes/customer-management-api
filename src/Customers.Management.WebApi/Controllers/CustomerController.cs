d771443c58f4899617a8dbfc2dc8f49026eb1113
Anderson Junkes <ojunkes@gmail.com>
2025-04-24 01:19:52 -0300

    Imlementação da controller de customer

--- src/Customers.Management.WebApi/Controllers/CustomerController.cs
+++ src/Customers.Management.WebApi/Controllers/CustomerController.cs
@@ -1,3 +1,6 @@
+using Customers.Management.Application.Requests;
+using Customers.Management.Application.Responses;
+using Customers.Management.Application.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Customer.Management.WebApi.Controllers
@@ -9,11 +12,64 @@ namespace Customer.Management.WebApi.Controllers
     {
         private const string RouteTemplate = "api/v1/customers";
 
-        [HttpGet]
-        public Task<bool> GetAll()
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
         {
-            return Task.FromResult(true);
+            _customerService = customerService;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken)
+        {
+            return Ok(await _customerService.GetAllCustomersAsync(cancellationToken));
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetCustomerAsync(Guid id, CancellationToken cancellationToken)
+        {
+            var customer = await _customerService.GetCustomerByIdAsync(id, cancellationToken);
+
+            return customer == null ? NoContent() : Ok(customer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> InsertCustomerAsync(CustomerInsertRequest request, CancellationToken cancellationToken)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var customer = await _customerService.InsertCustomerAsync(request, cancellationToken);
+
+            return customer == null ? BadRequest() : Ok(customer);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> UpdateCustomerAsync(Guid id, CustomerUpdateRequest request, CancellationToken cancellationToken)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != request.Id)
+            {
+                return BadRequest("Id da url não é o mesmo do Id do corpo da requisição.");
+            }
+
+            var customer = await _customerService.UpdateCustomerAsync(request, cancellationToken);
+
+            return customer == null ? BadRequest() : Ok(customer);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> DeleteCustomerAsync(Guid id, CancellationToken cancellationToken)
+        {
+            await _customerService.DeleteCustomerAsync(id, cancellationToken);
+
+            return Ok();
+        }
     }
 }
