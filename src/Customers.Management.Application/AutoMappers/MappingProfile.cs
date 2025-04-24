6b648b09a84e26aebd2829cd133206c5d1f6db22
Anderson Junkes <ojunkes@gmail.com>
2025-04-23 23:02:37 -0300

    Implementação do AutoMapper

---
+++ src/Customers.Management.Application/AutoMappers/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Customers.Management.Application.Requests;
+using Customers.Management.Application.Responses;
+using Customers.Management.Domain.Entities;
+
+namespace Customers.Management.Application.AutoMappers;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Customer, CustomerResponse>();
+        CreateMap<CustomerRequest, Customer>();
+    }
+}
--- src/Customers.Management.Application/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Customers.Management.Application/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Customers.Management.Infra.DependencyInjection;
+﻿using Customers.Management.Application.Services;
+using Customers.Management.Infra.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Customers.Management.Application.DependencyInjection;
@@ -7,6 +8,10 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection servicesCollection)
     {
+        servicesCollection.AddScoped<ICustomerService, CustomerService>();
+
+        servicesCollection.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
         return servicesCollection;
     }
 }
