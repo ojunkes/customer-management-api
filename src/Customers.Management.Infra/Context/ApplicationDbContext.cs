21c4d9a814bf0f7e2e0a3dd71c2a3823c780ac58
Anderson Junkes <ojunkes@gmail.com>
2025-04-23 21:23:29 -0300

    Aplicando carregamento automatico CreatedAt e ModifiedAt

--- src/Customers.Management.Infra/Context/ApplicationDbContext.cs
+++ src/Customers.Management.Infra/Context/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Customers.Management.Domain.Entities;
+using Customers.Management.Infra.Context.Abstraction;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
@@ -6,11 +7,36 @@ using System.Reflection;
 namespace Customers.Management.Infra.Context;
 
 [ExcludeFromCodeCoverage]
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : DbContext, IContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-    public DbSet<Customer> Customers { get; set; }
+    public async Task<bool> CommitAsync()
+    {
+        foreach (var entry in ChangeTracker.Entries()
+                                .Where(entry => entry.Entity.GetType().GetProperty("CreatedAt") != null))
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Property("CreatedAt").CurrentValue = DateTimeOffset.UtcNow;
+                    break;
+
+                case EntityState.Modified:
+                    entry.Property("ModifiedAt").CurrentValue = DateTimeOffset.UtcNow;
+                    entry.Property("CreatedAt").IsModified = false;
+                    break;
+            }
+        }
+
+        var result = await base.SaveChangesAsync();
+        return result > 0;
+    }
+
+    public DbSet<TEntity> GetDbSet<TEntity>() where TEntity : class
+    {
+        return Set<TEntity>();
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
