ca86c9cf5e32a5d718263fc8470a7997b5d0725a
Anderson Junkes <ojunkes@gmail.com>
2025-04-29 15:16:44 -0300

    Identação customerMapping

--- src/Customers.Management.Infra/Mappers/CustomerMapping.cs
+++ src/Customers.Management.Infra/Mappers/CustomerMapping.cs
@@ -2,58 +2,57 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Customers.Management.Infra.Mappers
+namespace Customers.Management.Infra.Mappers;
+
+internal class CustomerMapping : IEntityTypeConfiguration<Customer>
 {
-    internal class CustomerMapping : IEntityTypeConfiguration<Customer>
+    public void Configure(EntityTypeBuilder<Customer> builder)
     {
-        public void Configure(EntityTypeBuilder<Customer> builder)
-        {
-            builder.HasKey(c => c.Id);
-            builder.Property(c => c.Id)
-                .HasColumnType("UNIQUEIDENTIFIER");
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Id)
+            .HasColumnType("UNIQUEIDENTIFIER");
 
-            builder.Property(c => c.Name)
-                .IsRequired()
-                .HasMaxLength(60);
+        builder.Property(c => c.Name)
+            .IsRequired()
+            .HasMaxLength(60);
 
-            builder.Property(c => c.TaxId)
-                .IsRequired()
-                .HasMaxLength(11);
+        builder.Property(c => c.TaxId)
+            .IsRequired()
+            .HasMaxLength(11);
 
-            builder.HasIndex(c => c.TaxId)
-                .IsUnique();
+        builder.HasIndex(c => c.TaxId)
+            .IsUnique();
 
-            builder.Property(c => c.DateOfBirth)
-                .IsRequired();
+        builder.Property(c => c.DateOfBirth)
+            .IsRequired();
 
-            builder.Property(c => c.Street)
-                .IsRequired()
-                .HasMaxLength(100);
+        builder.Property(c => c.Street)
+            .IsRequired()
+            .HasMaxLength(100);
 
-            builder.Property(c => c.City)
-                .IsRequired()
-                .HasMaxLength(40);
+        builder.Property(c => c.City)
+            .IsRequired()
+            .HasMaxLength(40);
 
-            builder.Property(c => c.ZipCode)
-                .IsRequired()
-                .HasMaxLength(8);
+        builder.Property(c => c.ZipCode)
+            .IsRequired()
+            .HasMaxLength(8);
 
-            builder.Property(c => c.State)
-                .IsRequired()
-                .HasMaxLength(40);
+        builder.Property(c => c.State)
+            .IsRequired()
+            .HasMaxLength(40);
 
-            builder.Property(c => c.Country)
-                .IsRequired()
-                .HasMaxLength(30);
+        builder.Property(c => c.Country)
+            .IsRequired()
+            .HasMaxLength(30);
 
-            builder.Property(c => c.CreatedAt)
-                .IsRequired()
-                .HasColumnType("DATETIMEOFFSET");
+        builder.Property(c => c.CreatedAt)
+            .IsRequired()
+            .HasColumnType("DATETIMEOFFSET");
 
-            builder.Property(c => c.ModifiedAt)
-                .HasColumnType("DATETIMEOFFSET");
+        builder.Property(c => c.ModifiedAt)
+            .HasColumnType("DATETIMEOFFSET");
 
-            builder.ToTable("Customer");
-        }
+        builder.ToTable("Customer");
     }
 }
