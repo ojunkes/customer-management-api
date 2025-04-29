0294b70ae18a18a3cba5e8ccac047ee6603109b1
Anderson Junkes <ojunkes@gmail.com>
2025-04-29 14:34:19 -0300

    Criação da camada de consumer

---
+++ src/Customers.Management.Consumer/Consumers/ZipCodeMessageConsumer.cs
@@ -0,0 +1,20 @@
+﻿using Customers.Management.Domain.Messages;
+using MassTransit;
+
+namespace Customers.Management.Consumer.Consumers;
+
+public class ZipCodeMessageConsumer : IConsumer<ZipCodeMessage>
+{
+    public ZipCodeMessageConsumer()
+    {
+    }
+
+    public Task Consume(ConsumeContext<ZipCodeMessage> context)
+    {
+        var zipCode = context.Message.ZipCode;
+
+        Console.WriteLine($"[Worker] CEP recebido: {zipCode}");
+
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Customers.Management.Consumer/Program.cs
@@ -0,0 +1,40 @@
+using Customers.Management.Consumer;
+using Customers.Management.Consumer.Consumers;
+using Customers.Management.Infra.DependencyInjection;
+using MassTransit;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Customers.Management.Consumer;
+
+[ExcludeFromCodeCoverage]
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder(args);
+
+        builder.Services.AddDbContextSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")!);
+        builder.Services.AddRepositories();
+
+        builder.Services.AddMassTransit(x =>
+        {
+            x.AddConsumer<ZipCodeMessageConsumer>();
+
+            x.UsingRabbitMq((context, cfg) =>
+            {
+                var configuration = context.GetRequiredService<IConfiguration>();
+
+                cfg.Host(configuration["RabbitMq:Host"], "/", h =>
+                {
+                    h.Username(configuration["RabbitMq:Username"]!);
+                    h.Password(configuration["RabbitMq:Password"]!);
+                });
+
+                cfg.ConfigureEndpoints(context);
+            });
+        });
+
+        var host = builder.Build();
+        host.Run();
+    }
+}
