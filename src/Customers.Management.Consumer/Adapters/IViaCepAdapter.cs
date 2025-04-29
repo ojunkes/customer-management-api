4db5bff92691ff3caba8aab36e56842d0358c595
Anderson Junkes <ojunkes@gmail.com>
2025-04-29 19:38:09 -0300

    Implementação adapter ViaCep

---
+++ src/Customers.Management.Consumer/Adapters/IViaCepAdapter.cs
@@ -0,0 +1,8 @@
+﻿using Customers.Management.Consumer.Responses;
+
+namespace Customers.Management.Consumer.Adapters;
+
+public interface IViaCepAdapter
+{
+    Task<EnderecoResponse?> GetAddressByZipCodeAsync(string zipCode, CancellationToken cancellationToken);
+}
---
+++ src/Customers.Management.Consumer/Adapters/ViaCepAdapter.cs
@@ -0,0 +1,31 @@
+﻿using Customers.Management.Consumer.Responses;
+using System.Text.Json;
+
+namespace Customers.Management.Consumer.Adapters;
+
+public class ViaCepAdapter : IViaCepAdapter
+{
+    private readonly HttpClient _httpClient;
+    private const string BaseUrl = "https://viacep.com.br/ws/";
+
+    public ViaCepAdapter(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<EnderecoResponse?> GetAddressByZipCodeAsync(string zipCode, CancellationToken cancellationToken)
+    {
+        var response = await _httpClient.GetAsync($"{BaseUrl}{zipCode}/json/", cancellationToken);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync(cancellationToken);
+            return JsonSerializer.Deserialize<EnderecoResponse>(content, new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+        }
+
+        return null;
+    }
+}
