07ed312100468960742dd501dd5e615d55f7f536
Anderson Junkes <ojunkes@gmail.com>
2025-04-29 15:18:18 -0300

    Criaçaõ entidade/mapping Address

---
+++ src/Customers.Management.Domain/Entities/Address.cs
@@ -0,0 +1,52 @@
+﻿using Customers.Management.Domain.Core;
+
+namespace Customers.Management.Domain.Entities;
+
+public class Address : BaseEntity
+{
+    public string ZipCode { get; set; } = null!;
+    public string Street { get; set; } = null!;
+    public string Complement { get; set; } = null!;
+    public string Unit { get; set; } = null!;
+    public string Neighborhood { get; set; } = null!;
+    public string City { get; set; } = null!;
+    public string StateInitials { get; set; } = null!;
+    public string State { get; set; } = null!;
+    public string Region { get; set; } = null!;
+    public string IbgeCode { get; set; } = null!;
+    public string Gia { get; set; } = null!;
+    public string AreaCode { get; set; } = null!;
+    public string SiafiCode { get; set; } = null!;
+
+    public Address(
+        Guid id,
+        string zipCode,
+        string street,
+        string complement,
+        string unit,
+        string neighborhood,
+        string city,
+        string stateInitials,
+        string state,
+        string region,
+        string ibgeCode,
+        string gia,
+        string areaCode,
+        string siafiCode)
+    {
+        base.Id = id;
+        ZipCode = zipCode;
+        Street = street;
+        Complement = complement;
+        Unit = unit;
+        Neighborhood = neighborhood;
+        City = city;
+        StateInitials = stateInitials;
+        State = state;
+        Region = region;
+        IbgeCode = ibgeCode;
+        Gia = gia;
+        AreaCode = areaCode;
+        SiafiCode = siafiCode;
+    }
+}
---
+++ src/Customers.Management.Infra/Mappers/AddressMapping.cs
@@ -0,0 +1,30 @@
+﻿using Customers.Management.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Customers.Management.Infra.Mappers;
+
+internal class AddressMapping : IEntityTypeConfiguration<Address>
+{
+    public void Configure(EntityTypeBuilder<Address> builder)
+    {
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Id)
+            .HasColumnType("UNIQUEIDENTIFIER");
+
+        builder.Property(c => c.ZipCode)
+            .IsRequired();
+
+        builder.HasIndex(c => c.ZipCode)
+               .IsUnique();
+
+        builder.Property(c => c.CreatedAt)
+            .IsRequired()
+            .HasColumnType("DATETIMEOFFSET");
+
+        builder.Property(c => c.ModifiedAt)
+            .HasColumnType("DATETIMEOFFSET");
+
+        builder.ToTable("Address");
+    }
+}
