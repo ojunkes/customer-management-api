7c73002b1e00431cb988873faa4f8d84a084d7e0
Anderson Junkes <ojunkes@gmail.com>
2025-04-29 23:26:02 -0300

    Ajuste serealização

--- src/Customers.Management.WebApi/Program.cs
+++ src/Customers.Management.WebApi/Program.cs
@@ -3,6 +3,8 @@ using Customers.Management.Infra.DependencyInjection;
 using Customers.Management.Infra.Extensions;
 using Customers.Management.WebApi.Middlewares;
 using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Customers.Management.WebApi;
 
@@ -13,7 +15,13 @@ public static class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        builder.Services.AddControllers();
+        builder.Services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+                options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+            });
 
         builder.Services.AddEndpointsApiExplorer();
 
