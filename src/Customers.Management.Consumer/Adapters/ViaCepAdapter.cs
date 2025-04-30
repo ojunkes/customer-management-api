326333b6dc41b1408b3e1608abfff47d4ddf8651
Anderson Junkes <ojunkes@gmail.com>
2025-04-30 12:16:18 -0300

    Adicionando politica de retry no adapter

--- src/Customers.Management.Consumer/Adapters/ViaCepAdapter.cs
+++ src/Customers.Management.Consumer/Adapters/ViaCepAdapter.cs
@@ -1,31 +1,54 @@
 ﻿using Customers.Management.Consumer.Responses;
+using Polly;
+using Polly.Retry;
 using System.Text.Json;
 
 namespace Customers.Management.Consumer.Adapters;
 
 public class ViaCepAdapter : IViaCepAdapter
 {
+    private readonly ILogger<ViaCepAdapter> _logger;
     private readonly HttpClient _httpClient;
-    private const string BaseUrl = "https://viacep.com.br/ws/";
+    private readonly AsyncRetryPolicy<HttpResponseMessage> _retryPolicy;
 
-    public ViaCepAdapter(HttpClient httpClient)
+    public ViaCepAdapter(
+        ILogger<ViaCepAdapter> logger,
+        HttpClient httpClient)
     {
+        _logger = logger;
         _httpClient = httpClient;
+        _httpClient.BaseAddress = new Uri("https://viacep.com.br/ws/");
+
+        var retryDelays = new[]
+        {
+            TimeSpan.FromSeconds(2),
+            TimeSpan.FromSeconds(5),
+            TimeSpan.FromSeconds(10)
+        };
+
+        _retryPolicy = Policy<HttpResponseMessage>
+            .HandleResult(r => !r.IsSuccessStatusCode)
+            .WaitAndRetryAsync(
+                retryCount: retryDelays.Length,
+                sleepDurationProvider: retryAttempt => retryDelays[retryAttempt - 1],
+                onRetry: (outcome, timeSpan, retryCount, context) =>
+                {
+                    _logger.LogError("Tentativa {retryCount} após {timeSpan} segudos devido a: {result}", retryCount, timeSpan.TotalSeconds, outcome.Result);
+                });
     }
 
     public async Task<EnderecoResponse?> GetAddressByZipCodeAsync(string zipCode, CancellationToken cancellationToken)
     {
-        var response = await _httpClient.GetAsync($"{BaseUrl}{zipCode}/json/", cancellationToken);
+        var response = await _retryPolicy.ExecuteAsync(() =>
+            _httpClient.GetAsync($"{zipCode}/json/", cancellationToken));
 
-        if (response.IsSuccessStatusCode)
+        if (!response.IsSuccessStatusCode)
+            return null;
+
+        var content = await response.Content.ReadAsStringAsync(cancellationToken);
+        return JsonSerializer.Deserialize<EnderecoResponse>(content, new JsonSerializerOptions
         {
-            var content = await response.Content.ReadAsStringAsync(cancellationToken);
-            return JsonSerializer.Deserialize<EnderecoResponse>(content, new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true
-            });
-        }
-
-        return null;
+            PropertyNameCaseInsensitive = true
+        });
     }
 }
